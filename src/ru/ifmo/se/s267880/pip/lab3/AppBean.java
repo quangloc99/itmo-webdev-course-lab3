a67e602322499b765e1454e4953ebf4d439b0c6c
Tran Quang Loc <quangloctran99@gmail.com>
2019-11-13 22:45:07 +0300

    Add basic interactions with database.

--- src/ru/ifmo/se/s267880/pip/lab3/AppBean.java
+++ src/ru/ifmo/se/s267880/pip/lab3/AppBean.java
@@ -1,21 +1,49 @@
 package ru.ifmo.se.s267880.pip.lab3;
 
+import javax.annotation.PreDestroy;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Named;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 import java.io.Serializable;
+import java.util.List;
 
 @Named("app")
 @ApplicationScoped
 public class AppBean implements Serializable {
     private EditorBean editor = new EditorBean();
-    public AppBean() {}
+    private EntityManagerFactory checkHitQueryManagerFactory;
+    private EntityManager checkHitQueryManager;
+
+    public AppBean() {
+        checkHitQueryManagerFactory = Persistence.createEntityManagerFactory("Eclipselink_JPA");
+        checkHitQueryManager = checkHitQueryManagerFactory.createEntityManager();
+    }
+
+    @PreDestroy
+    public void destroy() {
+        checkHitQueryManager.close();
+        checkHitQueryManagerFactory.close();
+    }
 
     public EditorBean getEditor() {
         return editor;
     }
 
-    public Object ok() {
-        System.out.println("nice, ok");
-        return "ok";
+    public void addGeneratedQueriesToDB() {
+        checkHitQueryManager.getTransaction().begin();
+        editor.generateQueries().forEach(checkHitQueryManager::persist);
+        checkHitQueryManager.getTransaction().commit();
+    }
+
+    public List<CheckingHitQuery> getAllQueries() {
+        return (List<CheckingHitQuery>)checkHitQueryManager.createQuery("Select c from CheckingHitQuery c").getResultList();
+    }
+
+    public void deleteAllQueries() {
+        checkHitQueryManager.getTransaction().begin();
+        checkHitQueryManager.createNativeQuery("Delete From CheckingHitQuery").executeUpdate();
+        checkHitQueryManager.getTransaction().commit();
     }
 }
